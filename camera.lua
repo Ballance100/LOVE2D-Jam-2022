4013efb57ef07b0b4e42984b966a6b39cdac5099
Nathan <63228754+Nattie-G@users.noreply.github.com>
2022-03-08 15:02:21 +1100

    added ~33% slack to camera tolerances

--- camera.lua
+++ camera.lua
@@ -5,12 +5,18 @@ local Camera = {
  width= 608,
 }
 
-
 function Camera:update(target)
-  
-  local windowSize = window.width*window.tileScale
-  Camera.x = math.min( math.max( -target.x+windowSize/2, -(Camera.width-windowSize) ), 0)
-  
+  local windowSize = window.width*window.tileScale -- 200
+
+  local dxr = -target.x + 132
+  local dxl = -target.x + 56
+
+  --if target.x + Camera.x < -132 then
+  if Camera.x > dxr then
+    Camera.x = math.max(dxr, windowSize - Camera.width)
+  elseif Camera.x < dxl then
+    Camera.x = math.min(0, dxl)
+  end
 end
 
 return Camera
